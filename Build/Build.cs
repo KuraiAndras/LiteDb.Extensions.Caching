81d023d801df90fbb16ff745d0354e878737cf8c
Kurai András <k.andras9642@gmail.com>
2022-06-27 10:16:20 +0200

    implement build

--- Build/Build.cs
+++ Build/Build.cs
@@ -1,44 +1,73 @@
-using System;
-using System.Linq;
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Coverlet;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
+[GitHubActions
+(
+    "ci",
+    GitHubActionsImage.UbuntuLatest,
+    OnPullRequestBranches = new[] { "*" },
+    OnPushBranches = new[] { "main" },
+    InvokedTargets = new[] { nameof(Test) },
+    FetchDepth = 0
+)]
+[ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution = default!;
+    [GitVersion] readonly GitVersion GitVersion = default!;
+
+    AbsolutePath SourceDirectory => RootDirectory / "Source";
+    AbsolutePath TestsDirectory => RootDirectory / "Tests";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "Artifacts";
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
         });
 
     Target Restore => _ => _
+        .DependsOn(Clean)
         .Executes(() =>
-        {
-        });
+            DotNetRestore(s => s
+                .SetProjectFile(Solution)));
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
-        {
-        });
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore()));
 
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+            DotNetTest(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableCollectCoverage()
+                .SetCoverletOutputFormat(CoverletOutputFormat.opencover)));
 }
