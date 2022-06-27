276ae8929f890c75f16b59d576cc4e400720fd09
Kurai András <k.andras9642@gmail.com>
2022-06-27 16:37:08 +0200

    test multiple item removal, move registration tests to new class

--- Tests/Microsoft.Extensions.Caching.LiteDb.Tests/CacheTests.cs
+++ Tests/Microsoft.Extensions.Caching.LiteDb.Tests/CacheTests.cs
@@ -1,34 +1,10 @@
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.Caching.LiteDb;
 
 public class CacheTests
 {
-    [Fact]
-    public async Task Cache_Is_Registered()
-    {
-        // Arrange
-        const string dbPath = "TestDb";
-
-        var sp = new ServiceCollection()
-            .AddLiteDbCache(dbPath)
-            .BuildServiceProvider(true);
-
-        // Act
-        var cache = sp.GetService<IDistributedCache>();
-        var options = sp.GetService<IOptions<LiteDbCacheOptions>>()?.Value;
-
-        // Assert
-        cache.Should().NotBeNull();
-        options.Should().NotBeNull();
-
-        options!.CachePath.Should().Be(dbPath);
-
-        await sp.DisposeAsync();
-    }
-
     [Fact]
     public async Task Cached_Item_Can_Be_Retrieved()
     {
@@ -93,6 +69,32 @@ public class CacheTests
 
         // Assert
         result1.Should().Be(cacheValue1);
+        result2.Should().Be(cacheValue2);
+    }
+
+    [Fact]
+    public  async Task The_Right_Item_Is_Removed()
+    {
+        // Arrange
+        var cacheKey1 = Guid.NewGuid().ToString();
+        var cacheKey2 = Guid.NewGuid().ToString();
+        const string cacheValue1 = "Test1";
+        const string cacheValue2 = "Test1";
+
+        var sp = CreateProvider();
+        var cache = sp.GetRequiredService<IDistributedCache>();
+
+        // Act
+        await cache.SetStringAsync(cacheKey1, cacheValue1);
+        await cache.SetStringAsync(cacheKey2, cacheValue2);
+
+        await cache.RemoveAsync(cacheKey1);
+
+        var result1 = await cache.GetStringAsync(cacheKey1);
+        var result2 = await cache.GetStringAsync(cacheKey2);
+
+        // Assert
+        result1.Should().BeNullOrWhiteSpace();
         result2.Should().Be(cacheValue2);
     }
 
---
+++ Tests/Microsoft.Extensions.Caching.LiteDb.Tests/RegistrationTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Extensions.Caching.LiteDb;
+
+public class RegistrationTests
+{
+    [Fact]
+    public async Task Cache_Is_Registered()
+    {
+        // Arrange
+        const string dbPath = "TestDb";
+
+        var sp = new ServiceCollection()
+            .AddLiteDbCache(dbPath)
+            .BuildServiceProvider(true);
+
+        // Act
+        var cache = sp.GetService<IDistributedCache>();
+        var options = sp.GetService<IOptions<LiteDbCacheOptions>>()?.Value;
+
+        // Assert
+        cache.Should().NotBeNull();
+        options.Should().NotBeNull();
+
+        options!.CachePath.Should().Be(dbPath);
+
+        await sp.DisposeAsync();
+    }
+}
