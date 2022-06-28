c63db6cc2c61fc5a0d77ce47cc81bec2bc4eb4a9
Kurai András <k.andras9642@gmail.com>
2022-06-28 12:33:23 +0200

    add sliding refesh tests

--- Tests/Microsoft.Extensions.Caching.LiteDb.Tests/Cache_Expiry_Tests.cs
+++ Tests/Microsoft.Extensions.Caching.LiteDb.Tests/Cache_Expiry_Tests.cs
@@ -142,4 +142,68 @@ public sealed class Cache_Expiry_Tests : CacheTestBase
         value2.Should().Be(value1);
         value3.Should().Be(value2);
     }
+
+    [Fact]
+    public async Task Sliding_Expired_Refesh_Removes_Item()
+    {
+        // Arrange
+        var cacheKey = Guid.NewGuid().ToString();
+
+        // Act
+        await Cache.SetStringAsync(cacheKey, "Test", new DistributedCacheEntryOptions { SlidingExpiration = TimeSpan.FromMilliseconds(Expiration) });
+
+        await Task.Delay(Expiration * 2);
+
+        await Cache.RefreshAsync(cacheKey);
+
+        var value = await Cache.GetStringAsync(cacheKey);
+
+        // Assert
+        value.Should().BeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public async Task Absolute_With_Sliding_Expired_Is_Empty()
+    {
+        // Arrange
+        var cacheKey = Guid.NewGuid().ToString();
+        const int absoluteDelay = 300;
+
+        // Act
+        await Cache.SetStringAsync(cacheKey, "Test", new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMilliseconds(absoluteDelay),
+            SlidingExpiration = TimeSpan.FromMilliseconds(Expiration)
+        });
+
+        await Task.Delay((Expiration + absoluteDelay) * 2);
+
+        var value = await Cache.GetStringAsync(cacheKey);
+
+        // Assert
+        value.Should().BeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public async Task Absolute_With_Sliding_Not_Expired_Is_Not_Empty()
+    {
+        // Arrange
+        var cacheKey = Guid.NewGuid().ToString();
+        const string cacheValue = "Test";
+        const int absoluteDelay = 300;
+
+        // Act
+        await Cache.SetStringAsync(cacheKey, cacheValue, new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMilliseconds(absoluteDelay),
+            SlidingExpiration = TimeSpan.FromMilliseconds(Expiration)
+        });
+
+        await Task.Delay(Expiration / 2);
+
+        var value = await Cache.GetStringAsync(cacheKey);
+
+        // Assert
+        value.Should().Be(cacheValue);
+    }
 }
