7f6054a40fd2dcec5ef69b48d7f3689b7a5a958c
Kurai András <k.andras9642@gmail.com>
2022-08-02 10:02:45 +0200

    more registration tests

--- Sources/LiteDb.Extensions.Caching/ServiceCollectionExtensions.cs
+++ Sources/LiteDb.Extensions.Caching/ServiceCollectionExtensions.cs
@@ -16,8 +16,17 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
+    /// <summary>
+    /// Registers all LiteDbCache services
+    /// </summary>
+    /// <param name="services">services</param>
     public static IServiceCollection AddLiteDbCache(this IServiceCollection services) => services.AddLiteDbCache(_ => { });
 
+    /// <summary>
+    /// Registers all LiteDbCache services
+    /// </summary>
+    /// <param name="services">services</param>
+    /// <param name="setupAction">setup</param>
     public static IServiceCollection AddLiteDbCache(this IServiceCollection services, Action<LiteDbCacheOptions> setupAction)
     {
         if (services is null) throw new ArgumentNullException(nameof(services));
@@ -38,6 +47,11 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
+    /// <summary>
+    /// Registers all LiteDbCache services
+    /// </summary>
+    /// <param name="services">services</param>
+    /// <param name="path">db path</param>
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "Too much nesing")]
     public static IServiceCollection AddLiteDbCache(this IServiceCollection services, string path)
     {
@@ -47,6 +61,12 @@ public static class ServiceCollectionExtensions
         return AddLiteDbCache(services, options => options.CachePath = path);
     }
 
+    /// <summary>
+    /// Registers all LiteDbCache services
+    /// </summary>
+    /// <param name="services">services</param>
+    /// <param name="path">db path</param>
+    /// <param name="password">db password</param>
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "Too much nesing")]
     public static IServiceCollection AddLiteDbCache(this IServiceCollection services, string path, string password)
     {
